1c2e1a2c0882ed8dbfd097637cc48fd542deb375
arkhab <ersa1997@gmail.com>
2020-01-13 16:17:29 +0700

    latest

--- app/src/main/java/com/e/absenremotejti/MainActivity.java
+++ app/src/main/java/com/e/absenremotejti/MainActivity.java
@@ -1,10 +1,27 @@
 package com.e.absenremotejti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +36,63 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    TextInputEditText editTextId;
+    TextView textViewName;
+    Button buttonVerify;
+    Button buttonSignIn;
+    EditText editTextLocation;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference ListRef = db.collection("Employee_List");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextId = findViewById(R.id.text_input_serial_number);
+        textViewName = findViewById(R.id.employee_name);
+        buttonVerify = findViewById(R.id.btn_serial_number);
+        buttonSignIn = findViewById(R.id.btn_sign_in);
+        editTextLocation = findViewById(R.id.text_input_location_employee);
+
+        buttonVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ListRef.whereEqualTo("Id",editTextId).get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                for (DocumentSnapshot snapshot : task.getResult()){
+                                    textViewName.setText(snapshot.get("Name").toString());
+                                }
+                            }
+                        });
+            }
+        });
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Id = editTextId.getText().toString();
+                String Name = textViewName.getText().toString();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssz");
+                dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+                String Location = editTextLocation.getText().toString();
+
+                if (Id.trim().isEmpty() || Name.trim().isEmpty() || Location.trim().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please fill in the required information", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                upload(Id, Name, dateFormat, Location);
+            }
+        });
+
+    }
+
+    public void upload(final String Id, final String Name, final DateFormat dateFormat, final String Location){
+        CollectionReference reference = FirebaseFirestore.getInstance().collection("Employee_Attendance");
+        reference.add(new attendance(Id, Name, dateFormat, Location));
     }
 
     @Override
---
+++ app/src/main/java/com/e/absenremotejti/attendance.java
@@ -0,0 +1,59 @@
+package com.e.absenremotejti;
+
+import com.google.firebase.Timestamp;
+
+import java.text.DateFormat;
+
+public class attendance {
+
+    String Id;
+    String Name;
+    String Location;
+    Timestamp Date;
+
+    public String getLocation(){
+        return Location;
+    }
+
+    public void setLocation(String location){
+        Location = location;
+    }
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Timestamp getDate() {
+        return Date;
+    }
+
+    public void setDate(Timestamp date) {
+        Date = date;
+    }
+
+
+    public attendance(String id, String name, String location, Timestamp date) {
+        Id = id;
+        Name = name;
+        Date = date;
+        Location = location;
+    }
+
+    public attendance(String id, String name, DateFormat dateFormat, String location){
+
+    }
+
+
+
+}
